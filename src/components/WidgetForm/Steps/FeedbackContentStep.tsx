49642dedb9d1b6dca142479511c12ae28e608185
pceolato <pedroceolato2002@gmail.com>
2022-06-03 17:10:24 -0300

    feat: estilizando form

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,40 @@
-export function FeedbackContentStep () {
+import { ArrowLeft } from "phosphor-react";
+import { feedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+    feedbackType: feedbackType;
+    onRestartFeedbackRequest: () => void;
+}
+
+export function FeedbackContentStep ({ feedbackType, onRestartFeedbackRequest }: FeedbackContentStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType]
     
+    return (
+        <>
+            <header>
+                <button 
+                    type="button" 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" 
+                    onClick={onRestartFeedbackRequest}
+                    >
+                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                </button>
+
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+                    {feedbackTypeInfo.title}
+                </span>
+
+                <CloseButton />
+            </header>
+
+            <form className="my-4 w-full">
+                <textarea 
+                    className="min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
+                    placeholder="Conte com detalhes oque está acontecendo..."
+                />
+            </form>
+        </>
+    )
 }
